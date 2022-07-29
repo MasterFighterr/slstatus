4febdd8252596d5c84712c50f30e69255818d198
MasterFighterr <104787034+MasterFighterr@users.noreply.github.com>
2022-07-29 20:06:16 +0000

    Update config.h

--- config.h
+++ config.h
@@ -65,7 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ wifi_essid, "%s",   "wlan0",    },
+//	{ wifi_essid, "%s",   "wlan0",    },
 	{ battery_perc, "BATT %s%|",	    "BAT0",	},
 	{ cpu_perc, "CPU %s%|",     NULL,    },
 	{ ram_perc, "RAM %s%|",      NULL,    },
